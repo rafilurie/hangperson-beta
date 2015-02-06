b1716487a58f9e654486aba464d2e5556accb56a
saasbook <saasbook@saasbook.(none)>
2015-02-06 03:21:34 -0400

    4 tests left

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -13,10 +13,12 @@ class HangpersonGame
 	def guess(letter)
 		#Processes a guess and modifies the instance variables
 		#wrong_guesses and guesses accrodingly
-		if letter.empty?
+		if letter == ''
 			raise ArgumentError.new('You must guess something')
 		elsif letter == nil
 			raise ArgumentError.new('Cannot guess nil')
+		elsif !(letter =~ /^[a-z]+$/)
+			raise ArgumentError.new('Parameter must be a valid letter')
 		elsif !word.include? letter
 			if !wrong_guesses.include? letter
 				wrong_guesses << letter
@@ -32,17 +34,30 @@ class HangpersonGame
 		end
 	end
 
-	#def check_win_or_lose
+	def check_win_or_lose
 		#returns one of the symbols :win, :lose, or :play depending
 		#on the current game state
+		if wrong_guesses.length >= 7
+			return :lose
+		# elsif (@word.split('') & @guesses) == @guesses
+		#elsif (@guesses - @word.split("")).empty?
+		elsif @word.all? { |char| @guesses.include? char }
+		 	return :win
+		else
+			return :play
+		end
+	end
 
-	#end
-
-	#def word_with_guesses
+	def word_with_guesses()
 		#substitues the correct guesses made so far into the word
-		#regex @word.gsub(guesses list, '-')
-
-	#end
+		@copy = @word
+		@word_array = @copy.scan /\w/
+		# print @word_array
+		@letters = @word_array & @guesses
+		return @word.gsub(@letters, '-')
+		# word = 'banana'
+		#return @word)
+	end
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
--- spec/hangperson_game_spec.rb
+++ spec/hangperson_game_spec.rb
@@ -66,15 +66,15 @@ describe HangpersonGame do
       it 'throws an error when empty' do
         expect { @game.guess('') }.to raise_error(ArgumentError)
       end
-      it 'throws an error when not a letter', :pending => true do
+      it 'throws an error when not a letter' do
         expect { @game.guess('%') }.to raise_error(ArgumentError)
       end
-      it 'throws an error when nil', :pending => true do
+      it 'throws an error when nil' do
         expect { @game.guess(nil) }.to raise_error(ArgumentError)
       end
     end
   end
-  describe 'displayed word with guesses', :pending => true do
+  describe 'displayed word with guesses' do
     before :each do
       @game = HangpersonGame.new('banana')
     end
@@ -95,15 +95,15 @@ describe HangpersonGame do
     before :each do 
       @game = HangpersonGame.new('dog')
     end
-    it 'should be win when all letters guessed', :pending => true do
+    it 'should be win when all letters guessed' do
       guess_several_letters(@game, 'ogd')
       @game.check_win_or_lose.should == :win
     end
-    it 'should be lose after 7 incorrect guesses', :pending => true do
+    it 'should be lose after 7 incorrect guesses' do
       guess_several_letters(@game, 'tuvwxyz')
       @game.check_win_or_lose.should == :lose
     end
-    it 'should continue play if neither win nor lose', :pending => true do
+    it 'should continue play if neither win nor lose' do
       guess_several_letters(@game, 'do')
       @game.check_win_or_lose.should == :play
     end
