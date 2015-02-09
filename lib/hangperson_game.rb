6a2b5051a175b91feaa720701bcf03f7e6c91d9a
saasbook <saasbook@saasbook.(none)>
2015-02-09 01:38:03 -0400

    fixes

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -16,10 +16,13 @@ class HangpersonGame
 		adj_letter = letter.downcase
 		if adj_letter.length == 0
 			raise ArgumentError.new('You must guess something')
+			return false
 		elsif adj_letter == nil
 			raise ArgumentError.new('Cannot guess nil')
+			return false
 		elsif !(adj_letter =~ /^[a-z]+$/)
 			raise ArgumentError.new('Parameter must be a valid letter')
+			return false
 		elsif !word.include? adj_letter
 			if !wrong_guesses.include? adj_letter
 				wrong_guesses << adj_letter
