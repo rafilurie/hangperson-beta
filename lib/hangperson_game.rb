570b3056715e1525d6d6a6c39305c71cc593da89
saasbook <saasbook@saasbook.(none)>
2015-02-10 03:03:39 -0400

    sadfasca

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -13,14 +13,14 @@ class HangpersonGame
 	def guess(letter)
 		#Processes a guess and modifies the instance variables
 		#wrong_guesses and guesses accrodingly
-		if letter == "" or !(letter.downcase =~ /^[a-z]$/)
+		if letter == nil
+			raise ArgumentError.new('Cannot guess nil')
+			return false
+		elsif letter == "" or !(letter.downcase =~ /^[a-z]$/)
 			raise ArgumentError.new('You must guess something')
 			return false
 		elsif letter.length == 0
 			raise ArgumentError.new('Zero length')
-			return false
-		elsif letter == nil
-			raise ArgumentError.new('Cannot guess nil')
 			return false
 		# elsif !(letter.downcase =~ /^[a-z]$/)
 		# 	raise ArgumentError.new('Parameter must be a valid letter')
